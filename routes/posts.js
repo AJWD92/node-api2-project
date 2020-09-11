60d3739c8c1d8cf3b9e272084ba4ed52deb5901b
Aaron-Doran <wesley.gossett@gmail.com>
2020-09-11 04:52:23 -0400

    Refactored code to work with SQLite3 DB hit mvp.

--- routes/posts.js
+++ routes/posts.js
@@ -1,31 +1,116 @@
-const express = require('express')
-const {
-    getPosts,
-    getPost,
-    createPost,
-    updatePost,
-    deletePost
-} = require('../controllers/posts');
-
-const Post = require('../data/migrations/20180401213248_add_posts_table');
-
-// Include other resource routers
-const commentRouter = require('./comments');
+const express = require('express');
+const Posts = require('../data/db.js');
 
 const router = express.Router();
 
-// Re-router into other resources routers
-router.use('/:id/comments', commentRouter);
+// @desc    Gets all post
+// @route   GET /api/posts
+// @access  Public
+router.get('/', (req, res) => {
+    console.log(req.query);
+    Posts.find(req.query)
+        .then(posts => {
+            res.status(200).json({
+                success: true,
+                data: posts
+            })
+        })
+        .catch(error => {
+            res.status(500).json({
+                success: false,
+                error: 'The posts information could not be retrieved.'
+            });
+        });
+});
 
-router
-    .route('/')
-    .get(getPosts)
-    .post(createPost);
+// @desc    Gets a single post by id
+// @route   GET /api/posts/:id
+// @access  Public
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    Posts.findById(id)
+        .then(post => {
+            console.log(post);
+            if (post) {
+                res.status(200).json({
+                    success: true,
+                    data: post
+                }); 
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: 'The post with the specified ID does not exist.'
+                });
+            }
+            
+        })
+        .catch(error => {
+            res.status(500).json({
+                success: false,
+                error: 'The post information could not be retrieved.'
+            });
+        });
+});
 
-router
-    .route('/:id')
-    .get(getPost)
-    .put(updatePost)
-    .delete(deletePost);
+// @desc    Creates a new post
+// @route   POST /api/posts
+// @access  Public
+router.post('/', (req, res) => {
+    Posts.insert(req.body)
+        .then(post => {
+            res.status(201).json({
+                success: true,
+                data: post
+            })
+        })
+        .catch(error => {
+            res.status(500).json({
+                success: false,
+                error: 'Error'
+            });
+        });
+});
 
-module.exports = router;
+// @desc    Deletes a post by id
+// @route   DELETE /api/posts/:id
+// @access  Public
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Posts.remove(id)
+        .then(() => {
+            res.status(200).json({
+                success: true,
+                data: {}
+            });
+        })
+        .catch(error => {
+            res.status(500).json({
+                success: false,
+                error: 'Server error'
+            })
+        });
+});
+
+// @desc    Updates a post by id
+// @route   PUT /api/posts/:id
+// @access  Public
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Posts.update(id, req.body)
+        .then(postUpdated => {
+            res.status(200).json({
+                success: true,
+                data: postUpdated
+            });
+        })
+        .catch(error => {
+            res.status(500).json({
+                success: false,
+                error: 'Error'
+        })
+    })
+})
+
+module.exports = router
